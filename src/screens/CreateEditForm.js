5d7de95b1ba8145568e3889c1073f71f8831c615
nehakalagi <neha.kalagi1998@gmail.com>
2021-06-12 01:05:53 +0530

    first commit

--- src/screens/CreateEditForm.js
+++ src/screens/CreateEditForm.js
@@ -55,6 +55,8 @@ const validationSchema = Yup.object().shape({
 
 
 const decrypt = (password) => {
+  let bytes  = CryptoJS.AES.decrypt(password, 'secret key 123');
+  let originalText = bytes.toString(CryptoJS.enc.Utf8);
   return CryptoJS.AES.decrypt(password, 'secret key 123').toString();
 }
 
